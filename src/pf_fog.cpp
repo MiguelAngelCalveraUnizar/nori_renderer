9a08b45dff3b11dea9a11574f4d07dd72c62793b
MiguelAngelCalveraUnizar <113588655+MiguelAngelCalveraUnizar@users.noreply.github.com>
2023-01-11 18:10:45 +0100

    Update pf_fog.cpp

--- src/pf_fog.cpp
+++ src/pf_fog.cpp
@@ -15,26 +15,44 @@ class PF_Fog : public PhaseFunction {
 public:
     PF_Fog(const PropertyList &propList) {
         m_cte_albedo = Color3f(1,1,1); //White fog
-        //std::cout << " PF CREATEDDDD!!!!\n";
-        //m_radiance = new ConstantSpectrumTexture(props.getColor("radiance", Color3f(1.f)));
+        m_g = propList.getFloat("g", 0.0f);
     }
 
     /// Evaluate the BRDF model
     Color3f eval(const PFQueryRecord &bRec) const {
-        return m_cte_albedo * INV_FOURPI;
+        return m_cte_albedo * pdf(bRec);
     }
 
     /// Compute the density of \ref sample() wrt. solid angles
     float pdf(const PFQueryRecord&bRec) const {
-        // This will be INV_FOURPI
-        return Warp::squareToUniformSpherePdf(bRec.wo);
+        float g = m_g;
+        float denom = 1 + g * g + 2 * g * Frame::cosTheta(bRec.wo);
+        return INV_FOURPI * (1 - g * g) / (denom * std::sqrt(denom));
     }
 
     /// Draw a a sample from the BRDF model
     Color3f sample(PFQueryRecord&bRec, const Point2f &sample) const {
-        bRec.wo = Warp::squareToUniformSphere(sample);
-        float pdf_sample = pdf(bRec);
-        // m_cte_albedo / INV_FOUR_PI
+        float cosTheta;
+        float g = m_g;
+        if (std::abs(g) < 1e-3)
+            cosTheta = 1 - 2 * sample[0];
+        else {
+            float sqrTerm = (1 - g * g) /
+                (1 - g + 2 * g * sample[0]);
+            cosTheta = (1 + g * g - sqrTerm * sqrTerm) / (2 * g);
+        }   
+
+        float sinTheta = std::sqrt(std::max((float)0,
+            1 - cosTheta * cosTheta));
+        float phi = 2 * M_PI * sample[1];
+        
+        bRec.wo = Vector3f(sinTheta * std::cos(phi),
+            sinTheta * std::sin(phi),
+            cosTheta);
+
+        //float pdf_sample = pdf(bRec);
+        float pdf_sample = 1; //We assume we sample perfectly the values -> No need to weight the sample
+
         return eval(bRec) / pdf_sample;
     }
 
@@ -45,17 +63,15 @@ public:
     }
 
     void addChild(NoriObject* obj, const std::string& name = "none") {
-        switch (obj->getClassType()) {
-        default:
-            throw NoriException("Diffuse::addChild(<%s>) is not supported!",
+        throw NoriException("Diffuse::addChild(<%s>) is not supported!",
                 classTypeName(obj->getClassType()));
-        }
     }
 
 
     EClassType getClassType() const { return EPhaseFunction; }
 private:
     Color3f m_cte_albedo;
+    float m_g;
 };
 
 NORI_REGISTER_CLASS(PF_Fog, "pf_fog");
